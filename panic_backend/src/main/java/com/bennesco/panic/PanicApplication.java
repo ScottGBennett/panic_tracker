752a071a1eca7f9450a6415e4d4bbdc71556724a
ScottGBennett <scott.g.bennett@gmail.com>
2019-02-12 12:46:17 -0600

    Add project skeleton

---
+++ panic_backend/src/main/java/com/bennesco/panic/PanicApplication.java
@@ -0,0 +1,14 @@
+package com.bennesco.panic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PanicApplication.class, args);
+	}
+
+}
+
